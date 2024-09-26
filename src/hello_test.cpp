0d407c0632af8d0125b3d5367e42d139f0a9f49f
artyomthief <artyomsh628691@gmail.com>
2024-09-26 15:31:12 +0300

    Added OpenAPI specification

--- src/hello_test.cpp
+++ src/hello_test.cpp
@@ -3,8 +3,8 @@
 #include <userver/utest/utest.hpp>
 
 UTEST(SayHelloTo, Basic) {
-  using pg_service_template::SayHelloTo;
-  using pg_service_template::UserType;
+  using emergency_notification_system::SayHelloTo;
+  using emergency_notification_system::UserType;
 
   EXPECT_EQ(SayHelloTo("Developer", UserType::kFirstTime),
             "Hello, Developer!\n");
